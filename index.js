639121e74e9808f44c12b3a8907d2abc860322f9
Evan Losh <evand.losh@gmail.com>
2023-11-01 00:12:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "L" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+]
+
+function isWin(element, index, array) {
+    return element.result === "W";
+}
+
+function superbowlWin(x) {
+    if (x.find(isWin)) {
+        return ((x.find(isWin))).year;
+    }
+}
+
+console.log(superbowlWin(record));
